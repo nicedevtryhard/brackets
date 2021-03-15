af98b266b9231a384340f857996b0f05e20b9f03
Artsem <45534369+nicedevtryhard@users.noreply.github.com>
2021-03-15 21:36:34 +0300

    feat: add check func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let holder = [];
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == '(' ||
+            str[i] == '{' ||
+            str[i] == '[' ||
+            str[i] == '|' && holder[holder.length - 1] != '|' ||
+            str[i] == '7' && holder[holder.length - 1] != '7' ||
+            str[i] == '8' && holder[holder.length - 1] != '8' ||
+            str[i] == '1' && holder[holder.length - 1] != '2' ||
+            str[i] == '3' && holder[holder.length - 1] != '4' ||
+            str[i] == '5' && holder[holder.length - 1] != '6') {
+            holder.push(str[i]);
+        } else {
+            if (str[i] == ')' && holder[holder.length - 1] != '(' ||
+                str[i] == '}' && holder[holder.length - 1] != '{' ||
+                str[i] == '|' && holder[holder.length - 1] != '|') {
+                return false;
+            }
+            holder.pop();
+        }
+    }
+
+    return holder.length == 0;
 }
